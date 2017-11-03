4d2023f9f784d68fa6489b9539726747826b5d36
jiyeono <zzxcv1314@gmail.com>
2017-11-03 15:51:50 +0900

    sdf

--- UploadToServer.php
+++ UploadToServer.php
@@ -1,6 +1,6 @@
 <?php
   
-    $file_path = "VerifyFace/data";
+    $file_path = "VerifyFace/data/";
      
     $file_path = $file_path . basename( $_FILES['uploaded_file']['name']);
     if(move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $file_path)) {
