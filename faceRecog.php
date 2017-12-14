8f886776ae8613a63292373acea36b872808f2e9
zzxcv1314 <zzxcv1314@gmail.com>
2017-12-14 18:44:18 +0900

    sdfdsfdfsd

--- faceRecog.php
+++ faceRecog.php
@@ -88,7 +88,7 @@ function getFaceId(){
     //echo $json[0]['faceId'];
     $faceiid = $json[0]['faceId'];
     $faceatt = $json[0]['faceAttributes']['age'];
-    $faceemo = $json[0]['faceAttributes']['emotion'];
+    $faceemo = $json[0]['faceAttributes']['emotion'][1];
     
     echo $faceemo; 
     echo $faceatt; 
